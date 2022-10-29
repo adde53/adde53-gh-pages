59753da4d3623fb883e656fd95e1b50223bd9292
Andreas Olsson <andreas.olsson@sinch.com>
2022-10-29 20:02:44 +0200

    New functionality

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -5,30 +5,35 @@ function AboutMe() {
 return (
 	<div className="shopping-list">
               <ul>
-                  <div class="test2">
-                      <h2 class="addText">Mälardalens Tekniska Gymnasium</h2>
-                        <h1>MTG</h1>
-                      <h2 class="addText">Jun 2015 - Aug 2018</h2>
+                  <div class="testAboutMe">
+                      <h2 class="addTestAboutMe">Mälardalens Tekniska Gymnasium</h2>
+                        <h5>MTG</h5>
+                      <h2 class="addTestAboutMe">Aug 2015 - Jun 2018</h2>
                   </div>
-                  <div class="test2">
-                      <h2 class="addText">Underhållstekniker</h2>
-                        <h1>Scania</h1>
-                      <h2 class="addText">Jun 2018 - Aug 2018</h2>
+                  <div class="testAboutMe">
+                      <h2 class="addTestAboutMe">Underhållstekniker</h2>
+                        <h5>Scania</h5>
+                      <h2 class="addTestAboutMe">Jun 2018 - Aug 2018</h2>
                   </div>
-                  <div class="test2">
-                      <h2 class="addText">Kungliga Tekniska Högskolan</h2>
-                        <h1>KTH</h1>
-                      <h2 class="addText">Jun 2018 - Aug 2021</h2>
+                  <div class="testAboutMe">
+                      <h2 class="addTestAboutMe">Kungliga Tekniska Högskolan</h2>
+                        <h5>KTH</h5>
+                      <h2 class="addTestAboutMe">Aug 2018 - Jun 2021</h2>
                   </div>
-                  <div class="test2">
-                      <h2 class="addText">Johan i Hallen & Bergfalk </h2>
-                        <h1>JHB</h1>
-                      <h2 class="addText">Jun 2019 - Aug 2021</h2>
+                  <div class="testAboutMe">
+                      <h2 class="addTestAboutMe">Johan i Hallen & Bergfalk </h2>
+                        <h5>JHB</h5>
+                      <h2 class="addTestAboutMe">Jun 2019 - Aug 2021</h2>
                   </div>
-                  <div class="test2">
-                      <h2 class="addText">Mjukvaruutvecklare</h2>
-                        <h1>Sinch</h1>
-                      <h2 class="addText">Jun 2021 - nu</h2>
+                  <div class="testAboutMe">
+                      <h2 class="addTestAboutMe">Konsult hos Sinch</h2>
+                        <h5>Nexer</h5>
+                      <h2 class="addTestAboutMe">Aug 2021 - Aug 2022</h2>
+                  </div>
+                  <div class="testAboutMe">
+                      <h2 class="addTestAboutMe">Mjukvaruutvecklare</h2>
+                        <h5>Sinch</h5>
+                      <h2 class="addTestAboutMe">Aug 2021 - nu</h2>
                   </div>
               </ul>
     </div>
--- src/Shorts.js
+++ src/Shorts.js
@@ -4,143 +4,143 @@ import './App.css';
 function Shorts() {
 return (
 	<div className="shopping-list">
-              <h1>Shorts for Telecommunication and More</h1>
+              <h6>Shorts for Telecommunication and More</h6>
               <ul>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Charging Data Record</h1>
+                    <h2 class="addText">Charging Data Record</h2>
                       <h1>CDR</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">External Short Messaging Entity</h1>
+                    <h2 class="addText">External Short Messaging Entity</h2>
                       <h1>ESME</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">First Office Application</h1>
+                    <h2 class="addText">First Office Application</h2>
                       <h1>FOA</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">General Available</h1>
+                    <h2 class="addText">General Available</h2>
                       <h1>GA</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Home Location Register</h1>
+                    <h2 class="addText">Home Location Register</h2>
                       <h1>HLR</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Home Routing</h1>
+                    <h2 class="addText">Home Routing</h2>
                       <h1>HR</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Home Subscriber Server</h1>
+                    <h2 class="addText">Home Subscriber Server</h2>
                       <h1>HSS</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">IP Multimedia Subsystem</h1>
+                    <h2 class="addText">IP Multimedia Subsystem</h2>
                       <h1>IMS</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">International Mobile Subscriber Identity</h1>
+                    <h2 class="addText">International Mobile Subscriber Identity</h2>
                       <h1>IMSI</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Mobile Originated</h1>
+                    <h2 class="addText">Mobile Originated</h2>
                       <h1>MO</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Mobile Subscriber Integrated Services Digital Network</h1>
+                    <h2 class="addText">Mobile Subscriber Integrated Services Digital Network</h2>
                       <h1>MSISDN</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Mobile Terminated</h1>
+                    <h2 class="addText">Mobile Terminated</h2>
                       <h1>MT</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Online Charging System</h1>
+                    <h2 class="addText">Online Charging System</h2>
                       <h1>OSC</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Protocol Data Unit</h1>
+                    <h2 class="addText">Protocol Data Unit</h2>
                       <h1>PDU</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Subscriber Identify Module</h1>
+                    <h2 class="addText">Subscriber Identify Module</h2>
                       <h1>SIM</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Short Message Peer-to-Peer</h1>
+                    <h2 class="addText">Short Message Peer-to-Peer</h2>
                       <h1>SMPP</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Short Message Service</h1>
+                    <h2 class="addText">Short Message Service</h2>
                       <h1>SMS</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Short Message Service Center</h1>
+                    <h2 class="addText">Short Message Service Center</h2>
                       <h1>SMSC</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Send Ready ForSM</h1>
+                    <h2 class="addText">Send Ready ForSM</h2>
                       <h1>SRF</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Send Routing Info</h1>
+                    <h2 class="addText">Send Routing Info</h2>
                       <h1>SRI</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Translation Capabilities Application Part</h1>
+                    <h2 class="addText">Translation Capabilities Application Part</h2>
                       <h1>TCAP</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">User Data Header</h1>
+                    <h2 class="addText">User Data Header</h2>
                       <h1>UDH</h1>
                   </div>
                 </div>
                 <div class="main">
                   <div class="test">
-                    <h1 class="addText">Virtual Private Network</h1>
+                    <h2 class="addText">Virtual Private Network</h2>
                       <h1>VPN</h1>
                   </div>
                 </div>
